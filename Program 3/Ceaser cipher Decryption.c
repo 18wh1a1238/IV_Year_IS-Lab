21407cd02ba6fa5184986a3f29101a6abb877d20
18wh1a1238 <69301824+18wh1a1238@users.noreply.github.com>
2021-12-22 11:54:03 +0530

    Add files via upload

---
+++ Program 3/Ceaser cipher Decryption.c
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Input the plaintext message : ");
+        String plaintext = sc.nextLine();
+        System.out.println(" Enter the value by which each character in the plaintext message gets shifted : ");
+        int shift = sc.nextInt();
+        String ciphertext = "";
+        char alphabet;
+        for(int i=0; i < plaintext.length();i++) 
+        {
+             // Shift one character at a time
+            alphabet = plaintext.charAt(i);
+            
+            // if alphabet lies between a and z 
+            if(alphabet >= 'a'||alphabet >='A' && alphabet <= 'z' || alphabet <='Z') 
+            {
+             // shift alphabet
+             alphabet = (char) (alphabet - shift);
+             
+             }
+             ciphertext = ciphertext + alphabet;                
+    }
+    System.out.println(" ciphertext : " + ciphertext);
+  }
+}
